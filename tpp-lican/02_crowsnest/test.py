0d29263dc6a0797cdf77eb4453840d3997dfb860
Aaron <25444209+cli851@users.noreply.github.com>
2025-05-03 08:18:35 +0000

    rewriting test of 02

--- tpp-lican/02_crowsnest/test.py
+++ tpp-lican/02_crowsnest/test.py
@@ -18,31 +18,49 @@ template = 'Ahoy, Captain, {} {} off the larboard bow!'
 # --------------------------------------------------
 def test_exists():
     """exists"""
-    assert(os.path.exists(prg))
+    assert (os.path.exists(prg))
+
 
 # --------------------------------------------------
 def test_usage():
     """usage"""
-
+    for flag in ['-h', '--help']:
+        rv, out = getstatusoutput(f'{prg} {flag}')
+        assert rv == 0
+        assert out.lower().startswith('usage')
 
 
 # --------------------------------------------------
 def test_consonant():
     """brigantine -> a brigantine"""
-
+    for word in consonant_words:
+        rv, out = getstatusoutput(f'{prg} {word}')
+        assert rv == 0
+        assert out.strip() == template.format('a', word)
 
 
 # --------------------------------------------------
 def test_consonant_upper():
     """brigantine -> a Brigatine"""
+    for word in consonant_words:
+        rv, out = getstatusoutput(f'{prg} {word.title()}')
+        assert rv == 0
+        assert out.strip() == template.format('a', word.title())
 
 
 # --------------------------------------------------
 def test_vowel():
     """octopus -> an octopus"""
-
+    for word in vowel_words:
+        rv, out = getstatusoutput(f'{prg} {word}')
+        assert rv == 0
+        assert out.strip() == template.format('an', word)
 
 
 # --------------------------------------------------
 def test_vowel_upper():
     """octopus -> an Octopus"""
+    for word in vowel_words:
+        rv, out = getstatusoutput(f'{prg} {word.title()}')
+        assert rv == 0
+        assert out.strip() == template.format('an', word.title())
