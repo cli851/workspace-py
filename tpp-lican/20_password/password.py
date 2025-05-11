ab4dd7175014bfa99fedf541d843d4a24159b355
Aaron <lican101@gmail.com>
2025-05-11 18:38:12 +0800

    chapter 20 quickened

--- tpp-lican/20_password/password.py
+++ tpp-lican/20_password/password.py
@@ -6,6 +6,9 @@ Purpose: Rock the Casbah
 """
 
 import argparse
+import random
+import re
+import string
 
 
 # --------------------------------------------------
@@ -13,10 +16,73 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Password maker',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+    parser.add_argument('file',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),
+                        nargs='+',
+                        help='Input file(s)')
+
+    parser.add_argument('-n',
+                        '--num',
+                        metavar='num_passwords',
+                        type=int,
+                        default=3,
+                        help='Number of passwords to generate')
+
+    parser.add_argument('-w',
+                        '--num_words',
+                        metavar='num_words',
+                        type=int,
+                        default=4,
+                        help='Number of words to use for password')
+
+    parser.add_argument('-m',
+                        '--min_word_len',
+                        metavar='minimum',
+                        type=int,
+                        default=3,
+                        help='Minimum word length')
+
+    parser.add_argument('-x',
+                        '--max_word_len',
+                        metavar='maximum',
+                        type=int,
+                        default=6,
+                        help='Maximum word length')
+
+    parser.add_argument('-s',
+                        '--seed',
+                        metavar='seed',
+                        type=int,
+                        help='Random seed')
+
+    parser.add_argument('-l',
+                        '--l33t',
+                        action='store_true',
+                        help='Obfuscate letters')
+
     return parser.parse_args()
+
+
+def clean(word):
+    return re.sub('[^A-Za-z]', '', word)
+
+
+def ransom(text):
+    return ''.join(
+        map(lambda c: c.upper() if random.choice([0, 1]) else c.lower(), text)
+    )
+
+
+def l33t(text):
+    text = ransom(text)
+    xform = str.maketrans({
+        'a': '@', 'A': '4', 'O': '0', 't': '+', 'E': '3', 'I': '1', 'S': '5'
+    })
+    return text.translate(xform) + random.choice(string.punctuation)
 
 
 # --------------------------------------------------
