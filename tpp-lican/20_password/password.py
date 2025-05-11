186cc2f55423e6aeeb14e9f731aeb912f1623c37
Aaron <lican101@gmail.com>
2025-05-11 11:34:10 +0800

    chapter 20 init

---
+++ tpp-lican/20_password/password.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env py
+"""
+Author : Anonymous <Anonymous@localhost>
+Date   : 2025-05-10
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
